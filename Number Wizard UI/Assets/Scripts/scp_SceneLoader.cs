0c79c04cf6eb7f05e700b458ec9e135278e6306b
MarcoAntionioValerio <marco.valerio@leedscitycollege.ac.uk>
2019-04-26 12:23:50 +0100

    Created scene loader object and script, scenes hooked up in the index, button connected to the load next sceen method.

---
+++ Number Wizard UI/Assets/Scripts/scp_SceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scp_SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(CurrentSceneIndex + 1);
+        
+    }
+}
