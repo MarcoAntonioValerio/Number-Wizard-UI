aeb675ac3ddbf64c7c36994a6f83b7ab35693d45
MarcoAntionioValerio <marco.valerio@leedscitycollege.ac.uk>
2019-04-26 14:13:32 +0100

    Now the Scene Loader script counts to whatever i set, and then move to the next scene.

--- Number Wizard UI/Assets/Scripts/scp_SceneLoader.cs
+++ Number Wizard UI/Assets/Scripts/scp_SceneLoader.cs
@@ -5,10 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class scp_SceneLoader : MonoBehaviour
 {
+    
+    public float timer = 5f;
+    private float timeElapsed;
+
+    private void Update()
+    {
+        int CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        timeElapsed += Time.deltaTime;
+        if (CurrentSceneIndex == 0)
+        {
+            if (timeElapsed > timer)
+            {
+                SceneManager.LoadScene(CurrentSceneIndex + 1);
+            }
+        }
+        
+        
+    }
+
     public void LoadNextScene()
     {
         int CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(CurrentSceneIndex + 1);
         
     }
+    
+    
 }
